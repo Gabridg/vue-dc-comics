19aa602ef53d2c72028356cfa9079f83c8dbd36a
Gabridg <gabri99.del@gmail.com>
2022-07-22 16:21:43 +0200

    added link and img W/ no style

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <template>
   <div id="app">
-
+    <div class="container">
+      <Header></Header>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from "./components/HeaderComponent.vue";
 
 export default {
   name: 'App',
   components: {
+    Header,
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: "Segoe UI", "Open Sans", Helvetica, sans-serif;
+
+  .container {
+    max-width: 1200px;
+    margin: 0 auto;
+
+  }
 
 }
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,86 @@
 <template>
+    <figure>
+        <img src="../assets/img/dc-logo.png" alt="logo dc comics">
+    </figure>
+    <ul>
+        <li v-for="link in links" :key="link.id">
+            <a :href="link.url" :class="{ active: link.current }">{{ link.text }}"</a>
+        </li>
+    </ul>
 </template>
 
 <script>
+export default {
+    name: "HeaderComponent",
+    data() {
+        return {
+
+            links: [
+                {
+                    id: 1,
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    id: 2,
+                    text: 'Comics',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    id: 3,
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    id: 4,
+                    text: 'TV',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    id: 5,
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    id: 6,
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    id: 7,
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    id: 8,
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    id: 9,
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    id: 10,
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                },
+            ]
+
+        }
+    }
+}
 </script>
 
 <style>
